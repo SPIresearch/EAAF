15c05c2e674533ac6fe76163434ae04be689c011
SPI LAB@PKU <108445313+SPIresearch@users.noreply.github.com>
2023-10-23 16:57:47 +0800

    Update network.py

--- network.py
+++ network.py
@@ -153,37 +153,6 @@ class evidence_classifier(nn.Module):
         x=self.activation(x)
         return x
 
-class Res50(nn.Module):
-    def __init__(self):
-        super(Res50, self).__init__()
-        model_resnet = models.resnet50(pretrained=True)
-        self.conv1 = model_resnet.conv1
-        self.bn1 = model_resnet.bn1
-        self.relu = model_resnet.relu
-        self.maxpool = model_resnet.maxpool
-        self.layer1 = model_resnet.layer1
-        self.layer2 = model_resnet.layer2
-        self.layer3 = model_resnet.layer3
-        self.layer4 = model_resnet.layer4
-        self.avgpool = model_resnet.avgpool
-        self.in_features = model_resnet.fc.in_features
-        self.fc = model_resnet.fc
-        self.softplus=nn.Softplus()
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        y = self.fc(x)
-        x = self.softplus(x)
-        return x, y
-
 
 class scalar(nn.Module):
     def __init__(self, init_weights):
